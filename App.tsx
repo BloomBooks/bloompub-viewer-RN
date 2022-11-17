2101a7541456f38d8febb76fa3fceec3c90cae24
Gordon Martin <gordon_martin@sil.org>
2022-11-17 10:08:37 -0800

    Get a start
    * create .png forms of icons (RN doesn't do .svg)
    * get app rotating correctly

--- App.tsx
+++ App.tsx
@@ -2,9 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { BookReader } from './src/BookReader';
 import Constants from 'expo-constants';
+import { StartScreen } from './src/StartScreen';
 
-export default function App() {
+const App = () => {
     return (
+        <View style={styles.container}>
+            <StartScreen />
+            <StatusBar style="auto" />
+        </View>
         <SafeAreaView style={styles.safeAreaContainer}>
             <BookReader />
             <StatusBar />
@@ -12,9 +17,16 @@ export default function App() {
     );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
     safeAreaContainer: {
         height: "100%",
         paddingTop: Constants.statusBarHeight
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
     },
 });
