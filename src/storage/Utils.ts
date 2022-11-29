e9f99e38ed6b1274358942e8e7770ef0b2fe3e4f
Jeffrey Su <41497174+sujeffreyl@users.noreply.github.com>
2022-11-29 11:20:42 -0600

    Fix comprehension quiz sounds in Android

--- src/storage/Utils.ts
+++ src/storage/Utils.ts
@@ -11,3 +11,17 @@ export async function readAssetContentsAsync(
     }
     return FileSystem.readAsStringAsync(asset.localUri);
 }
+
+export async function copyAssetAsync(options: { asset: Asset; to: string }) {
+    const { asset, to } = options;
+    await asset.downloadAsync();
+
+    if (!asset.localUri) {
+        throw new Error("Asset localUri expected, but was null or empty.");
+    }
+
+    return FileSystem.copyAsync({
+        from: asset.localUri,
+        to,
+    });
+}
